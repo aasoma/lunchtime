f3f7288e96765b1910ec57344a6a0046937c7cba
aasoma <asoma2691@myhta.org>
2018-02-08 14:38:22 -1000

    finished

--- app.js
+++ app.js
@@ -7,46 +7,43 @@ var paragraph = document.getElementById("paragraph");
 var button = document.getElementById("button");
 
 var lunch1 = {
-imagesrc : "",
-name : "Cheseburger",
+imagesrc : "https://www.redrobin.com/content/dam/web/menu/2015-june/bacon-cheeseburger-1100.jpg",
+name : " Cheseburger",
 adjective : "12 Inch",
-verb : "Roller-Skating",
+verb : " Roller-Skating",
 };
 
 var lunch2 = {
-    imagesrc : "",
-    name : "Hot Dog",
+    imagesrc : "http://i1.ytimg.com/vi/tkXyM4Tzn7c/hqdefault.jpg",
+    name : " Hot Dog",
     adjective : "Juicy",
-    verb : "Surfing",
+    verb : " Surfing",
     };
 
 var lunch3 = {
-    imagesrc : "",
-    name : "Salad",
+    imagesrc : "https://recipes.heart.org/-/media/aha/recipe/recipe-images/mediterranean-salad.jpg",
+    name : " Salad",
     adjective : "Moist",
-    verb : "Swimming",
+    verb : " Swimming",
     };
 
 var lunch4 = {
-    imagesrc : "",
-    name : "Clam Chowder",
+    imagesrc : "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/08/01/clam-chowder-ck-1696572-x.jpg?itok=IZoSUuzS",
+    name : " Clam Chowder",
     adjective : "Thicc",
-    verb : "Swishing",
+    verb : " Swishing",
     };
 
 var lunch5 = {
-    imagesrc : "",
-    name : "Taco",
+    imagesrc : "http://img.taste.com.au/w_-0kcUJ/taste/2016/11/aussie-style-beef-and-salad-tacos-86525-1.jpeg",
+    name : " Taco",
     adjective : "Sexy",
-    verb : "Jumping",
+    verb : " Jumping",
      };
 
 var lunchOptions = [lunch1, lunch2, lunch3, lunch4, lunch5];     
 button.addEventListener("click", function(){
     var randomNumber = Math.floor((Math.random() *5) +0);
-    textArea.innerHTML= lunchOptions[randomNumber].name
-    
-});     
- 
-     
-
+    textArea.innerHTML= lunchOptions[randomNumber].adjective+lunchOptions[randomNumber].verb+lunchOptions[randomNumber].name
+    img.src = lunchOptions[randomNumber].imagesrc;
+});
